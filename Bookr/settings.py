51c48c6071e8c1cb0884fab491b1282e250760e4
sultankhan140 <sultnkhan1401s@gmail.com>
2024-08-05 17:53:31 +0530

    image

--- Bookr/settings.py
+++ Bookr/settings.py
@@ -123,12 +123,13 @@ USE_TZ = False  #by default it is True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'review/static'),
+    os.path.join(BASE_DIR, 'review/static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
